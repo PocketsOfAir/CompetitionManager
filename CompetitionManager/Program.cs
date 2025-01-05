c2fc5d664691c4ae984fb72513ac74c96a5dc3bc
Tom Cox <tcox2603@gmail.com>
2025-01-05 14:58:23 +1100

    Config errors no longer close the program without displaying or logging the error

--- CompetitionManager/Program.cs
+++ CompetitionManager/Program.cs
@@ -2,22 +2,29 @@
 using CompetitionManager.Transport;
 using CompetitionManager.Util;
 
-var teams = CsvUtils.LoadTeams();
-var completedRounds = CsvUtils.LoadCompletedRounds();
-var ratingUpdater = new RatingUpdateService(teams);
-ratingUpdater.UpdateRatingsForRounds(completedRounds);
-
-LoggingService.Instance.Log("");
-LoggingService.Instance.Log("Current Calculated Ratings");
-foreach (var team in teams.OrderByDescending(t => t.Rating))
+try
 {
-    LoggingService.Instance.Log($"    Team: {team.Name}, Rating: {team.Rating}");
-}
-LoggingService.Instance.Log("");
+    var teams = CsvUtils.LoadTeams();
+    var completedRounds = CsvUtils.LoadCompletedRounds();
+    var ratingUpdater = new RatingUpdateService(teams);
+    ratingUpdater.UpdateRatingsForRounds(completedRounds);
 
-var compConfig = JsonUtils.LoadCompetitionDetails();
-var matchupGenerator = MatchupStrategyFactory.GetMatchupEngine(compConfig);
-matchupGenerator.ExportMatches();
+    LoggingService.Instance.Log("");
+    LoggingService.Instance.Log("Current Calculated Ratings");
+    foreach (var team in teams.OrderByDescending(t => t.Rating))
+    {
+        LoggingService.Instance.Log($"    Team: {team.Name}, Rating: {team.Rating}");
+    }
+    LoggingService.Instance.Log("");
+
+    var compConfig = JsonUtils.LoadCompetitionDetails();
+    var matchupGenerator = MatchupStrategyFactory.GetMatchupEngine(compConfig);
+    matchupGenerator.ExportMatches();
+}
+catch (Exception e)
+{
+    LoggingService.Instance.Log($"Unexpected error generating matches: {e.Message}");
+}
 
 Console.Write("Press <Enter> to exit.");
 while (Console.ReadKey().Key != ConsoleKey.Enter) { }
